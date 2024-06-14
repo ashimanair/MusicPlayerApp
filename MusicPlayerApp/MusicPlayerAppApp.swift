92d2c07f88edbd87063e925081688257c4fd3c75
Ashima Nair <ashima.nair01@gmail.com>
2024-06-14 09:39:01 -0500

    Initial Commit

---
+++ MusicPlayerApp/MusicPlayerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerAppApp.swift
+//  MusicPlayerApp
+//
+//  Created by Scholar on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
